746d600dd63fb0e6bb8eec0cb9a7f50957147697
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-12-03 16:54:15 -0800

    Eighth commit, bringing in react spinner, creating a component and then setting that to load before our JSON server fetches the jobs data we need to display. When refreshing the page we can now see a loading symbol that appears before the data is actually displayed.

--- src/components/JobsListings.jsx
+++ src/components/JobsListings.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobsListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -9,7 +10,7 @@ const JobsListings = ({ isHome = false }) => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await fetchJobs("http://localhost:8000/jobs");
+        const res = await fetch("http://localhost:8000/jobs");
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -27,11 +28,16 @@ const JobsListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent jobs" : "Browse jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
