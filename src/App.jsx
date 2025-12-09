202027f01ae9c1530da343eb3587bf29099e917f
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-12-08 16:16:43 -0800

    Eleventh commit, Added the ability to add jobs via the form and make a POST request to the JSON server. Calling the add jobs function from the parent function of Apps.jsx. Additionally added the ability DELETE forms with a DELETE method and navigate back to the jobs page after. Added a confirm window to ensure you want to actually delete the job.

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,6 @@ import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="add-job" element={<AddJobPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
 
 // const App = () => {
 //   return <RouterProvider router={router} />;
@@ -30,6 +19,45 @@ const router = createBrowserRouter(
 // export default App;
 
 const App = () => {
+  //add new job
+  const addJob = async (newJob) => {
+    const res = await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  //delete job
+  const deleteJob = async function (id) {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route
+          path="/add-job"
+          element={<AddJobPage addJobsSubmit={addJob} />}
+        />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,23 +1,46 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AddJobPage = () => {
+const AddJobPage = ({ addJobsSubmit }) => {
   const [title, setTitle] = useState("");
-  const [type, setType] = useState("");
+  const [type, setType] = useState("Full-Time");
   const [location, setLocation] = useState("");
   const [description, setDescription] = useState("");
-  const [salary, setSalary] = useState("");
+  const [salary, setSalary] = useState("Under $50K");
   const [companyName, setCompanyName] = useState("");
   const [companyDescription, setCompanyDescription] = useState("");
   const [contactEmail, setCompanyEmail] = useState("");
   const [contactPhone, setContactPhone] = useState("");
+
+  const navigate = useNavigate();
+
+  const submitForm = function (e) {
+    e.preventDefault();
+
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone,
+      },
+    };
+    addJobsSubmit(newJob);
+    return navigate("/jobs");
+  };
 
   return (
     <>
       <section className="bg-indigo-50">
         <div className="container m-auto max-w-2xl py-24">
           <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-            <form>
+            <form onSubmit={submitForm}>
               <h2 className="text-3xl text-center font-semibold mb-6">
                 Add Job
               </h2>
